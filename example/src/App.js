34745dc0e5c70bfde8a68067d9ec742e28fe2503
Leonardo Cardoso <leocardosoti@gmail.com>
2019-10-10 19:28:27 -0300

    updatet example

--- example/src/App.js
+++ example/src/App.js
@@ -6,10 +6,13 @@ export default class App extends Component {
   state = { value: 0 };
   render() {
     return (
-      <BeautyStars
-        value={this.state.value}
-        onChange={value => this.setState({ value })}
-      />
+      <div className="wrapper">
+        <h1 style={{ textAlign: 'center', color: '#fff' }}>Beauty Stars</h1>
+        <BeautyStars
+          value={this.state.value}
+          onChange={value => this.setState({ value })}
+        />
+      </div>
     );
   }
 }
