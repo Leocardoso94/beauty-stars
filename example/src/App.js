7d3abdb8c9c8e58b6f4aa2ae2b8588c0d9bb0201
Leonardo Cardoso <leo@reformma.com.br>
2019-10-10 19:03:28 -0300

    improve readme (#1)

--- example/src/App.js
+++ example/src/App.js
@@ -6,12 +6,10 @@ export default class App extends Component {
   state = { value: 0 };
   render() {
     return (
-      <div>
-        <BeautyStars
-          value={this.state.value}
-          onChange={value => this.setState({ value })}
-        />
-      </div>
+      <BeautyStars
+        value={this.state.value}
+        onChange={value => this.setState({ value })}
+      />
     );
   }
 }
